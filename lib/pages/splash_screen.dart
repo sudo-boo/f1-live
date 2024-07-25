b45325cbbf69dc42599398f5dacc2707f9096e9a
sudo-boo <22b0910@iitb.ac.in>
2024-07-25 19:17:13 +0530

    ADD: home page and splash_screen

---
+++ lib/pages/splash_screen.dart
@@ -0,0 +1,34 @@
+// splash_screen.dart
+
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(milliseconds: 1200), () {
+      Navigator.pushReplacementNamed(context, '/home');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF28282b),
+      body: Center(
+        child: Image.asset(
+          'assets/images/splash.png',
+          height: screenHeight(context),
+        ),
+      ),
+    );
+  }
+}
